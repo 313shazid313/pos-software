e11efbf37426e6433b22106debda279c588c04dc
313shazid313 <hossainshazid567@gmail.com>
2024-11-13 22:54:15 +0600

    dash board updated

---
+++ cliant/src/App.jsx
@@ -0,0 +1,11 @@
+import DashboardLayoutBasic from "./MaterialUI/DashboardLayoutBasic";
+
+function App() {
+  return (
+    <div >
+      <DashboardLayoutBasic />
+    </div>
+  );
+}
+
+export default App;
